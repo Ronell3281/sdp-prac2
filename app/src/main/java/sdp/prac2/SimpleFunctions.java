52b58e58a49bf6ca9db19c490af53e6d8d8cfce2
JayMaurice24 <100361234+JayMaurice24@users.noreply.github.com>
2022-07-21 16:49:01 +0200

    Update SimpleFunctions.java

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -18,6 +18,28 @@ public class SimpleFunctions {
 
     }
 =======
+     public List<Integer> Task2(List<Integer> list) {
+        int size = list.size(); //gets size of list
+        if (list.isEmpty()){
+            return list; //returns list if it's empty
+        }
+        else{
+            for(int i =0; i<size;i++){ //loops through each element in list
+                int element = list.get(i); //gets element at index
+                String s = Integer.toString(element); //converts into to string
+                if (s.length()==1){ //if lement only has one character it removes it from list 
+                    list.remove(i);
+                }
+                else{
+                    String split = s.substring(1, s.length() -1); //splits character from second character to end
+                    int newElement = Integer.parseInt(split); // converts split string back to integer
+                    list.add(i, newElement); //adds element at specified position
+                }
+        }
+            return list;
+
+     }
+    }
 
     public static List<Integer> Task4(List<Integer> a, List<Integer> b)
     {
