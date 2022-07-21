45b5277ebd1440a4569e774aff20d1c8af6c71b5
siy9 <shlomendlini82@gmail.com>
2022-07-21 15:31:11 +0200

    Task 4 update

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,24 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static List<Integer> Task4(List<Integer> a, List<Integer> b)
+    {
+        List<Integer> c =new ArrayList<Integer>();  
+
+        if(a.size() == b.size())
+        {
+            int listLength = a.size();
+            int halfListSize = 0;
+            
+            for(int i=0; i < listLength; i++)
+            {
+                c.add(a.get(i) + b.get(listLength - 1 - i));
+            }
+            return c;
+        }
+        
+        return null;
+    }
+
 }
