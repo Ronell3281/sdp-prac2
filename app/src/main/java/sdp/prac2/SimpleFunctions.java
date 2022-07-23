d7ca2d69805b870e434af3da18d614917846fdfa
Odwantsumane <109234613+Odwantsumane@users.noreply.github.com>
2022-07-23 14:54:49 +0200

    Task5 uptodate

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 
 public class SimpleFunctions {
-    public static void main(String[] args) {
-        System.out.println(Task5());
-    }
+   // public static void main(String[] args) {
+   //     System.out.println(Task5());
+   // }
     public SimpleFunctions() {}
 
 
